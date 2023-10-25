232775c36e98b9bbd5eb4b1418efe3b1d3562309
Aman Kumar Kulshreshtha <aman.kumar@smartData-250.local>
2023-10-25 13:24:42 +0530

    update

--- Assignment.js
+++ Assignment.js
@@ -10,10 +10,34 @@ function Assignment() {
         padding: 10,
       }}>
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <View style={{backgroundColor: 'red', height: 50, width: 50}}></View>
-        <View style={{backgroundColor: 'red', height: 50, width: 50}}></View>
-        <View style={{backgroundColor: 'red', height: 50, width: 50}}></View>
-        <View style={{backgroundColor: 'red', height: 50, width: 50}}></View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            height: 50,
+            width: 50,
+            borderRadius: 10,
+          }}></View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            height: 50,
+            width: 50,
+            borderRadius: 10,
+          }}></View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            height: 50,
+            width: 50,
+            borderRadius: 10,
+          }}></View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            height: 50,
+            width: 50,
+            borderRadius: 10,
+          }}></View>
       </View>
 
       <View
@@ -21,8 +45,15 @@ function Assignment() {
           flexDirection: 'row',
           justifyContent: 'space-between',
         }}>
-        <View style={{backgroundColor: 'red', flex: 0.46}}></View>
-        <View style={{backgroundColor: 'red', height: 50, width: 50}}></View>
+        <View
+          style={{backgroundColor: 'red', flex: 0.46, borderRadius: 10}}></View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            height: 50,
+            width: 50,
+            borderRadius: 10,
+          }}></View>
       </View>
 
       <View style={{flexDirection: 'row', justifyContent: 'flex-end', gap: 65}}>
@@ -31,16 +62,31 @@ function Assignment() {
             backgroundColor: 'red',
             height: 50,
             width: 170,
+            borderRadius: 10,
           }}></View>
 
-        <View style={{backgroundColor: 'red', height: 50, width: 50}}></View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            height: 50,
+            width: 50,
+            borderRadius: 10,
+          }}></View>
       </View>
 
-      <View style={{backgroundColor: 'red', flex: 0.5}}></View>
+      <View
+        style={{backgroundColor: 'red', flex: 0.5, borderRadius: 10}}></View>
 
-      <View style={{backgroundColor: 'red', flex: 0.2}}></View>
+      <View
+        style={{backgroundColor: 'red', flex: 0.2, borderRadius: 10}}></View>
 
-      <View style={{backgroundColor: 'red', flex: 0.1, borderRadius: 1}}></View>
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 0.1,
+          borderRadius: 1,
+          borderRadius: 10,
+        }}></View>
     </View>
   );
 }
